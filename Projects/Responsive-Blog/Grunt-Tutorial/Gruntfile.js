0f17c2bbeef5292568ba816fc6448f290411e86c
guti7 <7505855+guti7@users.noreply.github.com>
2018-01-12 23:10:52 -0800

    Update Gruntfile with tasks

--- Projects/Responsive-Blog/Grunt-Tutorial/Gruntfile.js
+++ Projects/Responsive-Blog/Grunt-Tutorial/Gruntfile.js
@@ -13,12 +13,27 @@ module.exports = function(grunt) {
         src: 'js/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js', 'js/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
     }
   });
 
   // Load the plugins that provide tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
-}
+
+};
